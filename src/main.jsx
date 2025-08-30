834ced32a0e37de4f15227b40c244241d2955494
eaa24martia <eaa24martia@gmail.com>
2025-08-30 11:33:57 +0200

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-router-spa/"}>
       <App />
     </BrowserRouter>
   </StrictMode>
